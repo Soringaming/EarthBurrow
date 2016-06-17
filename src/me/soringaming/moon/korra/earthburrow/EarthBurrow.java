4a1c7d2dccfd698e937fdaa7099f29d81c283865
Moon243 <Gideonssolock@gmail.com>
2016-06-17 16:12:40 -0700

    Added Combo shit

--- src/me/soringaming/moon/korra/earthburrow/EarthBurrow.java
+++ src/me/soringaming/moon/korra/earthburrow/EarthBurrow.java
@@ -1,23 +1,26 @@
 package me.soringaming.moon.korra.earthburrow;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
-import org.bukkit.util.Vector;
+import org.bukkit.permissions.PermissionDefault;
 
+import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.ComboAbility;
 import com.projectkorra.projectkorra.ability.EarthAbility;
 import com.projectkorra.projectkorra.ability.util.ComboManager.AbilityInformation;
+import com.projectkorra.projectkorra.util.ClickType;
 
-public class EarthBurrow extends EarthAbility implements ComboAbility{
-	
+public class EarthBurrow extends EarthAbility implements ComboAbility {
+
 	private Player player;
 	private Location loc;
 	private Permission perm;
 	private Location start;
-	
+
 	public EarthBurrow(Player player) {
 		super(player);
 		this.player = player;
@@ -32,14 +35,27 @@ public class EarthBurrow extends EarthAbility implements ComboAbility{
 
 	@Override
 	public Location getLocation() {
-		return null;
+		return loc;
 	}
 
 	@Override
 	public String getName() {
-		return null;
+		return "EarthBurrow";
 	}
 
+	private String getVersion() {
+		return "v1.0";
+	}
+	
+	private String getAuthor() {
+		return "Moon243 & Soringaming";
+	}
+	
+	
+	public String getDescripton() {
+		return "Test Ability";
+	}
+	
 	@Override
 	public boolean isHarmlessAbility() {
 		return false;
@@ -52,22 +68,43 @@ public class EarthBurrow extends EarthAbility implements ComboAbility{
 
 	@Override
 	public void progress() {
-		
+
 	}
 
 	@Override
-	public Object createNewComboInstance(Player arg0) {
-		return null;
+	public Object createNewComboInstance(Player player) {
+		return new EarthBurrow(player);
 	}
 
 	@Override
 	public ArrayList<AbilityInformation> getCombination() {
-		return null;
+		ArrayList<AbilityInformation> list = new ArrayList<AbilityInformation>();
+		list.add(new AbilityInformation("Collapse", ClickType.RIGHT_CLICK));
+		list.add(new AbilityInformation("EarthTunnel", ClickType.LEFT_CLICK));
+		list.add(new AbilityInformation("EarthTunnel", ClickType.LEFT_CLICK));
+		return list;
 	}
 
 	@Override
 	public String getInstructions() {
-		return null;
+		return "Collapse > (Right-Click) > EarthTunnel (Left-Click) > EarthTunnel (Left-Click)";
+	}
+	
+	
+
+	public void doLoad() {
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By " + getAuthor() + "Has Been Enabled. ");
+		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new EarthBurrowListener(), ProjectKorra.plugin);
+		perm = new Permission("bending.ability.EarthBurrow");
+		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
+		perm.setDefault(PermissionDefault.TRUE);
 	}
 
+	
+	public void stop() {
+		ProjectKorra.plugin.getServer().getPluginManager().removePermission(perm);
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By " + getAuthor() + " Has Been Disabled.");
+		super.remove();
+	}
+	
 }
