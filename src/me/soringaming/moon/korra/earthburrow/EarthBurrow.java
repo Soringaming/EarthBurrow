f24747593c2e0e351de2cd03c1e1512c00713bb5
Moon243 <Gideonssolock@gmail.com>
2016-06-17 15:33:35 -0700

    Added variables

--- src/me/soringaming/moon/korra/earthburrow/EarthBurrow.java
+++ src/me/soringaming/moon/korra/earthburrow/EarthBurrow.java
@@ -4,69 +4,69 @@ import java.util.ArrayList;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.util.Vector;
 
 import com.projectkorra.projectkorra.ability.ComboAbility;
 import com.projectkorra.projectkorra.ability.EarthAbility;
 import com.projectkorra.projectkorra.ability.util.ComboManager.AbilityInformation;
 
 public class EarthBurrow extends EarthAbility implements ComboAbility{
-
+	
+	private Player player;
+	private Location loc;
+	private Permission perm;
+	private Location start;
+	
 	public EarthBurrow(Player player) {
 		super(player);
-		// TODO Auto-generated constructor stub
+		this.player = player;
+		this.loc = player.getLocation();
+		this.start = player.getLocation();
 	}
 
 	@Override
 	public long getCooldown() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public Location getLocation() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public boolean isHarmlessAbility() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public boolean isSneakAbility() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public void progress() {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public Object createNewComboInstance(Player arg0) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public ArrayList<AbilityInformation> getCombination() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public String getInstructions() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
