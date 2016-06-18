a47b225b49c6aea82ff5fd695c51d637ad649e8d
Soringaming <thehawksquad50@gmail.com>
2016-06-18 12:10:37 +1000

    Made changes

--- src/me/soringaming/moon/korra/earthburrow/EarthBurrow.java
+++ src/me/soringaming/moon/korra/earthburrow/EarthBurrow.java
@@ -14,28 +14,29 @@ import org.bukkit.util.Vector;
 
 import com.projectkorra.projectkorra.GeneralMethods;
 import com.projectkorra.projectkorra.ProjectKorra;
+import com.projectkorra.projectkorra.ability.AddonAbility;
 import com.projectkorra.projectkorra.ability.ComboAbility;
 import com.projectkorra.projectkorra.ability.EarthAbility;
 import com.projectkorra.projectkorra.ability.util.ComboManager.AbilityInformation;
 import com.projectkorra.projectkorra.util.ClickType;
 import com.projectkorra.projectkorra.util.TempBlock;
 
-public class EarthBurrow extends EarthAbility implements ComboAbility {
+public class EarthBurrow extends EarthAbility implements AddonAbility, ComboAbility {
 
 	private static final ConcurrentHashMap<Block, Long> getTopBlock = new ConcurrentHashMap<Block, Long>();
 
-	private Player player;
+	private Player p;
 	private Location loc;
 	private Permission perm;
-	private Location start;
 	private boolean StoredTopBlock;
 
 	private int depth;
+
 	public EarthBurrow(Player player) {
 		super(player);
-		this.player = player;
-		this.loc = player.getLocation();
-		this.start = player.getLocation();
+		this.p = player;
+		this.loc = p.getLocation();
+		start();
 	}
 
 	@Override
@@ -53,19 +54,10 @@ public class EarthBurrow extends EarthAbility implements ComboAbility {
 		return "EarthBurrow";
 	}
 
-	private String getVersion() {
-		return "v1.0";
-	}
-	
-	private String getAuthor() {
-		return "Moon243 & Soringaming";
-	}
-	
-	
 	public String getDescripton() {
 		return "Test Ability";
 	}
-	
+
 	@Override
 	public boolean isHarmlessAbility() {
 		return false;
@@ -86,7 +78,8 @@ public class EarthBurrow extends EarthAbility implements ComboAbility {
 			remove();
 			return;
 		}
-		
+		doBlockRemove();
+
 	}
 
 	public void doBlockRemove() {
@@ -101,6 +94,9 @@ public class EarthBurrow extends EarthAbility implements ComboAbility {
 			b.setType(Material.AIR);
 			player.setVelocity(new Vector(0, -4, 0));
 			depth++;
+		} else {
+			remove();
+			return;
 		}
 
 	}
@@ -117,7 +113,12 @@ public class EarthBurrow extends EarthAbility implements ComboAbility {
 
 	@Override
 	public Object createNewComboInstance(Player player) {
-		return new EarthBurrow(player);
+		return new EarthBurrow(p);
+	}
+
+	@Override
+	public boolean isHiddenAbility() {
+		return true;
 	}
 
 	@Override
@@ -133,22 +134,35 @@ public class EarthBurrow extends EarthAbility implements ComboAbility {
 	public String getInstructions() {
 		return "Collapse > (Right-Click) > EarthTunnel (Left-Click) > EarthTunnel (Left-Click)";
 	}
-	
-	
 
-	public void doLoad() {
-		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By " + getAuthor() + "Has Been Enabled. ");
-		ProjectKorra.plugin.getServer().getPluginManager().registerEvents(new EarthBurrowListener(), ProjectKorra.plugin);
+	@Override
+	public void stop() {
+		ProjectKorra.plugin.getServer().getPluginManager().removePermission(perm);
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO,
+				getName() + " " + getVersion() + " Developed By " + getAuthor() + " Has Been Disabled.");
+		super.remove();
+	}
+
+	@Override
+	public void load() {
+		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO,
+				getName() + " " + getVersion() + " Developed By " + getAuthor() + "Has Been Enabled. ");
 		perm = new Permission("bending.ability.EarthBurrow");
 		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
 		perm.setDefault(PermissionDefault.TRUE);
+
 	}
 
-	
-	public void stop() {
-		ProjectKorra.plugin.getServer().getPluginManager().removePermission(perm);
-		ProjectKorra.plugin.getServer().getLogger().log(Level.INFO, getName() + " " + getVersion() + " Developed By " + getAuthor() + " Has Been Disabled.");
-		super.remove();
+	@Override
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return "Soringaming&Moon";
 	}
-	
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return "v1.0";
+	}
+
 }
--- src/me/soringaming/moon/korra/earthburrow/EarthBurrowListener.java
+++
@@ -1,7 +0,0 @@
-package me.soringaming.moon.korra.earthburrow;
-
-import org.bukkit.event.Listener;
-
-public class EarthBurrowListener implements Listener{
-
-}
